cdcc1c7ed14764164cc35e922fef5450f6994eef
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2026-03-26 17:57:50 +0600

    add new path and add the sum functionlites using param

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from "express";
+import sum from "./sum.js"
 const app = express();
 const PORT = 3030;
 
-app.get("/me",(req,res) =>{
+
+app.get("/me",async(req,res) =>{
     res.send("server is running ,Ismail")
+})
+
+app.get("/sum/:a/:b",async(req,res) =>{
+    const {a,b} = req.params;
+    const result = sum(parseInt(a),parseInt(b));
+    res.send(`sum of ${a} and ${b} is ${result}`);
 })
 
 app.listen(PORT,() =>{
